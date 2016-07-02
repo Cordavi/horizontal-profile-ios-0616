33fcfa892455349a789cc05e4452f3024abc084b
Cordavi <michaelamundsen@gmail.com>
2016-07-02 17:25:26 -0400

    Added size class layouts for custom landscape views

--- horizontal-profile/ProfileViewController.m
+++ horizontal-profile/ProfileViewController.m
@@ -23,76 +23,20 @@
 
 - (void)viewDidLoad {
     [super viewDidLoad];
-    //UIImage *coverPhotoPlaceholder = [UIImage imageNamed:@"coverPhotoDefault.png"];
-    //self.coverImage = [[UIImageView alloc] initWithImage:coverPhotoPlaceholder];
-    
-    [self.view removeConstraints:self.view.constraints];
     self.view.translatesAutoresizingMaskIntoConstraints = NO;
-    [self.nameLabel removeConstraints:self.nameLabel.constraints];
     self.coverImage.translatesAutoresizingMaskIntoConstraints = NO;
     self.profileImage.translatesAutoresizingMaskIntoConstraints = NO;
     self.nameLabel.translatesAutoresizingMaskIntoConstraints = NO;
     self.classNameLabel.translatesAutoresizingMaskIntoConstraints = NO;
     self.interestingFactLabel.translatesAutoresizingMaskIntoConstraints = NO;
     self.profileTextView.translatesAutoresizingMaskIntoConstraints = NO;
-    
-    
-    //coverImage
-    [self.coverImage.topAnchor constraintEqualToAnchor:self.view.topAnchor].active = YES;
-    [self.coverImage.leftAnchor constraintEqualToAnchor:self.view.leftAnchor].active = YES;
-    [self.coverImage.rightAnchor constraintEqualToAnchor:self.view.rightAnchor].active = YES;
-    [self.coverImage.heightAnchor constraintEqualToAnchor:self.view.heightAnchor multiplier:0.15].active = YES;
-    
-    
-    //profile picture
-    [self.profileImage.topAnchor constraintEqualToAnchor:self.coverImage.bottomAnchor constant:40].active = YES;
-    [self.profileImage.bottomAnchor constraintEqualToAnchor:self.profileTextView.topAnchor constant:-40].active = YES;
-    [self.profileImage.leftAnchor constraintEqualToAnchor:self.profileTextView.leftAnchor constant:5].active = YES;
-    [self.profileImage.widthAnchor constraintEqualToAnchor:self.profileTextView.widthAnchor multiplier:0.35].active = YES;
-    
-    
-    //Profile textField
-    [self.profileTextView.widthAnchor constraintEqualToAnchor:self.view.widthAnchor multiplier:0.90].active = YES;
-    [self.profileTextView.heightAnchor constraintEqualToAnchor:self.view.heightAnchor multiplier:0.50].active = YES;
-    [self.profileTextView.centerXAnchor constraintEqualToAnchor:self.view.centerXAnchor].active = YES;
-    [self.profileTextView.bottomAnchor constraintEqualToAnchor:self.view.bottomAnchor].active = YES;
-    
-    //name
-    [self.nameLabel.topAnchor constraintEqualToAnchor:self.profileImage.topAnchor].active = YES;
-    [self.nameLabel.leftAnchor constraintEqualToAnchor:self.profileImage.rightAnchor constant:20].active = YES;
-    [self.nameLabel.rightAnchor constraintEqualToAnchor:self.profileTextView.rightAnchor constant:-5].active = YES;
-    
-    //[self.nameLabel.bottomAnchor constraintEqualToAnchor:self.classNameLabel.topAnchor constant:-20].active = YES;
-    //[self.nameLabel.heightAnchor constraintEqualToAnchor:self.profileImage.heightAnchor multiplier:0.33].active = YES;
-    //[self.nameLabel.widthAnchor constraintEqualToAnchor:self.profileTextView.widthAnchor multiplier:0.55].active = YES;
-    
-    //class
-    [self.classNameLabel.leftAnchor constraintEqualToAnchor:self.profileImage.rightAnchor constant:20].active = YES;
-    [self.classNameLabel.rightAnchor constraintEqualToAnchor:self.profileTextView.rightAnchor constant:-5].active = YES;
-    [self.classNameLabel.centerYAnchor constraintEqualToAnchor:self.profileImage.centerYAnchor].active = YES;
-    //[self.classNameLabel.bottomAnchor constraintEqualToAnchor:self.interestingFactLabel.topAnchor constant:-20].active = YES;
-    //[self.classNameLabel.topAnchor constraintEqualToAnchor:self.nameLabel.bottomAnchor constant:20].active = YES;
-    //[self.classNameLabel.widthAnchor constraintEqualToAnchor:self.profileTextView.widthAnchor multiplier:0.55].active = YES;
-    //[self.classNameLabel.heightAnchor constraintEqualToAnchor:self.profileImage.heightAnchor multiplier:0.33].active = YES;
-    
-    
-    
-    //fact
-    [self.interestingFactLabel.leftAnchor constraintEqualToAnchor:self.profileImage.rightAnchor constant:20].active = YES;
-    [self.interestingFactLabel.rightAnchor constraintEqualToAnchor:self.profileTextView.rightAnchor constant:-5].active = YES;
-    [self.interestingFactLabel.bottomAnchor constraintEqualToAnchor:self.profileImage.bottomAnchor].active = YES;
-    //[self.interestingFactLabel.topAnchor constraintEqualToAnchor:self.classNameLabel.bottomAnchor constant:20].active = YES;
-    //[self.interestingFactLabel.widthAnchor constraintEqualToAnchor:self.profileTextView.widthAnchor multiplier:0.55].active = YES;
-    //[self.interestingFactLabel.heightAnchor constraintEqualToAnchor:self.profileImage.heightAnchor multiplier:0.33].active = YES;
-    
-    
-    
-    
-    
+    [self removeConstraints];
+    [self setConstraints];
 }
 
-- (void)viewWillTransitionToSize:(CGSize)size withTransitionCoordinator:(id<UIViewControllerTransitionCoordinator>)coordinator {
-    
+- (void)traitCollectionDidChange:(UITraitCollection *)previousTraitCollection {
+    [self removeConstraints];
+    [self setConstraints];
 }
 
 - (void)didReceiveMemoryWarning {
@@ -100,14 +44,118 @@
     // Dispose of any resources that can be recreated.
 }
 
-/*
- #pragma mark - Navigation
- 
- // In a storyboard-based application, you will often want to do a little preparation before navigation
- - (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
- // Get the new view controller using [segue destinationViewController].
- // Pass the selected object to the new view controller.
- }
- */
+- (void)removeConstraints {
+    [self.view removeConstraints:self.view.constraints];
+    [self.coverImage removeConstraints:self.coverImage.constraints];
+    [self.profileImage removeConstraints:self.profileImage.constraints];
+    
+}
+
+- (void)setConstraints {
+    if (self.view.traitCollection.horizontalSizeClass == UIUserInterfaceSizeClassRegular) {
+        //shows cover image
+        self.coverImage.hidden = NO;
+        
+        //coverImage
+        [self.coverImage.topAnchor constraintEqualToAnchor:self.view.topAnchor].active = YES;
+        [self.coverImage.heightAnchor constraintEqualToAnchor:self.view.heightAnchor multiplier:0.15].active = YES;
+        [self.coverImage.widthAnchor constraintEqualToAnchor:self.view.widthAnchor].active = YES;
+        
+        //profile pic
+        [self.profileImage.topAnchor constraintEqualToAnchor:self.coverImage.bottomAnchor constant:20].active = YES;
+        [self.profileImage.bottomAnchor constraintEqualToAnchor:self.view.bottomAnchor constant:-20].active = YES;
+        [self.profileImage.leadingAnchor constraintEqualToAnchor:self.view.leadingAnchor constant:20].active = YES;
+        [self.profileImage.widthAnchor constraintEqualToAnchor:self.view.widthAnchor multiplier:0.3].active = YES;
+        
+        //profile text
+        [self.profileTextView.topAnchor constraintEqualToAnchor:self.coverImage.bottomAnchor constant:20].active = YES;
+        [self.profileTextView.bottomAnchor constraintEqualToAnchor:self.view.bottomAnchor constant:-20].active = YES;
+        [self.profileTextView.trailingAnchor constraintEqualToAnchor:self.view.trailingAnchor constant:-20].active = YES;
+        [self.profileTextView.widthAnchor constraintEqualToAnchor:self.view.widthAnchor multiplier:0.4].active = YES;
+        
+        //name
+        [self.nameLabel.topAnchor constraintEqualToAnchor:self.profileImage.topAnchor].active = YES;
+        [self.nameLabel.leftAnchor constraintEqualToAnchor:self.profileImage.rightAnchor constant:20].active = YES;
+        [self.nameLabel.rightAnchor constraintEqualToAnchor:self.profileTextView.leftAnchor constant:-20].active = YES;
+        
+        //class
+        [self.classNameLabel.leftAnchor constraintEqualToAnchor:self.profileImage.rightAnchor constant:20].active = YES;
+        [self.classNameLabel.rightAnchor constraintEqualToAnchor:self.profileTextView.leftAnchor constant:-20].active = YES;
+        [self.classNameLabel.centerYAnchor constraintEqualToAnchor:self.profileImage.centerYAnchor].active = YES;
+        
+        //fact
+        [self.interestingFactLabel.leftAnchor constraintEqualToAnchor:self.profileImage.rightAnchor constant:20].active = YES;
+        [self.interestingFactLabel.rightAnchor constraintEqualToAnchor:self.profileTextView.leftAnchor constant:-20].active = YES;
+        [self.interestingFactLabel.bottomAnchor constraintEqualToAnchor:self.profileImage.bottomAnchor].active = YES;
+        
+        
+    } else if (self.view.traitCollection.verticalSizeClass == UIUserInterfaceSizeClassRegular) {
+        //shows cover image
+        self.coverImage.hidden = NO;
+        
+        //coverImage
+        [self.coverImage.topAnchor constraintEqualToAnchor:self.view.topAnchor].active = YES;
+        [self.coverImage.heightAnchor constraintEqualToAnchor:self.view.heightAnchor multiplier:0.15].active = YES;
+        [self.coverImage.widthAnchor constraintEqualToAnchor:self.view.widthAnchor].active = YES;
+        
+        //profile text
+        [self.profileTextView.heightAnchor constraintEqualToAnchor:self.view.heightAnchor multiplier:0.50].active = YES;
+        [self.profileTextView.bottomAnchor constraintEqualToAnchor:self.view.bottomAnchor constant:-20].active = YES;
+        [self.profileTextView.leadingAnchor constraintEqualToAnchor:self.view.leadingAnchor constant:20].active = YES;
+        [self.profileTextView.trailingAnchor constraintEqualToAnchor:self.view.trailingAnchor constant:-20].active = YES;
+        
+        //profile pic
+        [self.profileImage.topAnchor constraintEqualToAnchor:self.coverImage.bottomAnchor constant:20].active = YES;
+        [self.profileImage.bottomAnchor constraintEqualToAnchor:self.profileTextView.topAnchor constant:-20].active = YES;
+        [self.profileImage.leadingAnchor constraintEqualToAnchor:self.profileTextView.leadingAnchor].active = YES;
+        [self.profileImage.widthAnchor constraintEqualToAnchor:self.view.widthAnchor multiplier:0.4].active = YES;
+        
+        //name
+        [self.nameLabel.topAnchor constraintEqualToAnchor:self.profileImage.topAnchor].active = YES;
+        [self.nameLabel.leftAnchor constraintEqualToAnchor:self.profileImage.rightAnchor constant:20].active = YES;
+        [self.nameLabel.rightAnchor constraintEqualToAnchor:self.profileTextView.rightAnchor constant:-5].active = YES;
+        
+        //class
+        [self.classNameLabel.leftAnchor constraintEqualToAnchor:self.profileImage.rightAnchor constant:20].active = YES;
+        [self.classNameLabel.rightAnchor constraintEqualToAnchor:self.profileTextView.rightAnchor constant:-5].active = YES;
+        [self.classNameLabel.centerYAnchor constraintEqualToAnchor:self.profileImage.centerYAnchor].active = YES;
+        
+        //fact
+        [self.interestingFactLabel.leftAnchor constraintEqualToAnchor:self.profileImage.rightAnchor constant:20].active = YES;
+        [self.interestingFactLabel.rightAnchor constraintEqualToAnchor:self.profileTextView.rightAnchor constant:-5].active = YES;
+        [self.interestingFactLabel.bottomAnchor constraintEqualToAnchor:self.profileImage.bottomAnchor].active = YES;
+    } else if (self.view.traitCollection.verticalSizeClass == UIUserInterfaceSizeClassCompact) {
+        //hides cover image
+        self.coverImage.hidden = YES;
+        
+        //profile pic
+        [self.profileImage.topAnchor constraintEqualToAnchor:self.view.topAnchor constant:20].active = YES;
+        [self.profileImage.bottomAnchor constraintEqualToAnchor:self.profileTextView.topAnchor constant:-20].active = YES;
+        [self.profileImage.leadingAnchor constraintEqualToAnchor:self.profileTextView.leadingAnchor].active = YES;
+        [self.profileImage.widthAnchor constraintEqualToAnchor:self.view.widthAnchor multiplier:0.25].active = YES;
+        
+        //profile text
+        [self.profileTextView.heightAnchor constraintEqualToAnchor:self.view.heightAnchor multiplier:0.5].active = YES;
+        [self.profileTextView.bottomAnchor constraintEqualToAnchor:self.view.bottomAnchor constant:-20].active = YES;
+        [self.profileTextView.leadingAnchor constraintEqualToAnchor:self.view.leadingAnchor constant:20].active = YES;
+        [self.profileTextView.trailingAnchor constraintEqualToAnchor:self.view.trailingAnchor constant:-20].active = YES;
+        
+        //name
+        [self.nameLabel.topAnchor constraintEqualToAnchor:self.profileImage.topAnchor].active = YES;
+        [self.nameLabel.leftAnchor constraintEqualToAnchor:self.profileImage.rightAnchor constant:20].active = YES;
+        [self.nameLabel.rightAnchor constraintEqualToAnchor:self.profileTextView.rightAnchor constant:-10].active = YES;
+        
+        //class
+        [self.classNameLabel.leftAnchor constraintEqualToAnchor:self.profileImage.rightAnchor constant:20].active = YES;
+        [self.classNameLabel.rightAnchor constraintEqualToAnchor:self.profileTextView.rightAnchor constant:-10].active = YES;
+        [self.classNameLabel.centerYAnchor constraintEqualToAnchor:self.profileImage.centerYAnchor].active = YES;
+        
+        //fact
+        [self.interestingFactLabel.leftAnchor constraintEqualToAnchor:self.profileImage.rightAnchor constant:20].active = YES;
+        [self.interestingFactLabel.rightAnchor constraintEqualToAnchor:self.profileTextView.rightAnchor constant:-10].active = YES;
+        [self.interestingFactLabel.bottomAnchor constraintEqualToAnchor:self.profileImage.bottomAnchor].active = YES;
+        
+    }
+}
 
 @end
